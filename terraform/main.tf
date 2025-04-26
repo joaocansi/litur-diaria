8a1af855a6e9140ee44e14502964fda5189f9e20
joaocansi <whitshs2003@gmail.com>
2025-04-25 21:48:59 -0300

    feat: add vercel.json file

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,11 @@ terraform {
 }
 
 provider "vercel" {
-  api_token = var.vercel_api_url
+  api_token = var.vercel_api_key
 }
 
 provider "stripe" {
-  api_key = var.stripe_api_key
+  api_key = var.stripe_secret_key
 }
 
 resource "vercel_project" "client" {
@@ -27,24 +27,93 @@ resource "vercel_project" "client" {
     repo = "joaocansi/liturgia-diaria"
     type = "github"
   }
+
+  root_directory = "client"
+}
+
+resource "vercel_project_environment_variables" "client_vars" {
+  project_id = vercel_project.client.id
+
+  variables = [
+    {
+      key    = "NEXT_PUBLIC_BASE_URL"
+      value  = var.client_url
+      target = ["production", "preview"]
+    },
+    {
+      key    = "NEXTAUTH_URL"
+      value  = var.client_url
+      target = ["production", "preview"]
+    },
+    {
+      key    = "NEXTAUTH_SECRET"
+      value  = var.nextauth_secret
+      target = ["production", "preview"]
+    },
+    {
+      key    = "GOOGLE_CLIENT_ID"
+      value  = var.google_client_id
+      target = ["production", "preview"]
+    },
+    {
+      key    = "GOOGLE_CLIENT_SECRET"
+      value  = var.google_client_secret
+      target = ["production", "preview"]
+    },
+    {
+      key    = "STRIPE_SECRET_KEY"
+      value  = var.stripe_secret_key
+      target = ["production", "preview"]
+    },
+    {
+      key    = "STRIPE_WEBHOOK_SECRET"
+      value  = var.stripe_webhook_secret
+      target = ["production", "preview"]
+    },
+    {
+      key    = "STRIPE_PRICE_ID"
+      value  = var.stripe_price_id
+      target = ["production", "preview"]
+    },
+    {
+      key    = "AWS_ACCESS_KEY_ID"
+      value  = var.aws_access_key_id
+      target = ["production", "preview"]
+    },
+    {
+      key    = "AWS_SECRET_ACCESS_KEY"
+      value  = var.aws_secret_access_key
+      target = ["production", "preview"]
+    },
+    {
+      key    = "AWS_REGION"
+      value  = var.aws_region
+      target = ["production", "preview"]
+    }
+  ]
 }
 
 resource "vercel_project_domain" "client_domain" {
   project_id = vercel_project.client.id
-  domain     = "liturgia.joaocansi.site"
+  domain     = var.client_domain
 }
 
-resource "stripe_webhook_endpoint" "webhook" {
-  url         = var.client_url
-  description = "service webhook"
-  enabled_events = [
-    "checkout.session.completed",
-    "invoice.paid",
-    "invoice.payment_failed"
-  ]
+resource "vercel_deployment" "client_deploy" {
+  project_id = vercel_project.client.id
+  ref = "main"
 }
 
-output "webhook_secret" {
-  value     = stripe_webhook_endpoint.webhook.secret
-  sensitive = true
-}
+# resource "stripe_webhook_endpoint" "webhook" {
+#   url         = var.client_url
+#   description = "service webhook"
+#   enabled_events = [
+#     "checkout.session.completed",
+#     "invoice.paid",
+#     "invoice.payment_failed"
+#   ]
+# }
+
+# output "webhook_secret" {
+#   value     = stripe_webhook_endpoint.webhook.secret
+#   sensitive = true
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,62 @@
 variable "stripe_api_key" {
   type    = string
-  default = ""
+  sensitive = true
 }
 
-variable "vercel_api_url" {
+variable "vercel_api_key" {
   type    = string
-  default = ""
+  sensitive = true
+}
+
+variable "client_domain" {
+  type    = string
+  sensitive = true
 }
 
 variable "client_url" {
   type    = string
-  default = ""
+  sensitive = true
+}
+
+variable "google_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "google_client_id" {
+  type = string
+  sensitive = true
+}
+
+variable "nextauth_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_webhook_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_price_id" {
+  type = string
+}
+
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_region" {
+  type = string
 }
