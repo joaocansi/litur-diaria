47480f828ead3af6856957faab873cc86f4b6ed6
joaocansi <whitshs2003@gmail.com>
2025-04-30 23:12:21 -0300

    fix: remove theme class

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light", enableColorScheme: false }}>
+        <Providers>
           {children}
         </Providers>
       </body>
