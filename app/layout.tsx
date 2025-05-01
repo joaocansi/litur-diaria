5bbfad12caf756697a83d09194b0cb80f5725964
joaocansi <whitshs2003@gmail.com>
2025-05-01 17:41:58 -0300

    fix: change amber color to amber-300

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
       >
         <Providers themeProps={{ 
             attribute: "class", 
-            defaultTheme: "dark", 
+            defaultTheme: "light", 
         }}>
           {children}
         </Providers>
